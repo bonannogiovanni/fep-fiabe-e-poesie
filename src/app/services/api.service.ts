6c3cbba681962275f5e3cbb59d32e7b0ef92c769
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-09-18 05:11:39 +0200

    changed base url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
+// const BASE_URL = "http://localhost:3000"
+
+const BASE_URL = "https://fep-json-server.herokuapp.com/"
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,22 +13,22 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   createKid(kid: any) {
-    return this.http.post<any>('http://localhost:3000/bambini/', kid);
+    return this.http.post<any>(`${BASE_URL}/bambini/`, kid);
   }
 
   updateKid(kid: any, id:number) {
-    return this.http.put<any>(`http://localhost:3000/bambini/${id}`, kid);
+    return this.http.put<any>(`${BASE_URL}/bambini/${id}`, kid);
   }
 
   getKids() {
-    return this.http.get<any>('http://localhost:3000/bambini/');
+    return this.http.get<any>(`${BASE_URL}/bambini/`);
   }
 
   getKid(id: number) {
-    return this.http.get<any>(`http://localhost:3000/bambini/${id}`);
+    return this.http.get<any>(`${BASE_URL}/bambini/${id}`);
   }
 
   deleteKid(id: number) {
-    return this.http.delete<any>(`http://localhost:3000/bambini/${id}`);
+    return this.http.delete<any>(`${BASE_URL}/bambini/${id}`);
   }
 }
