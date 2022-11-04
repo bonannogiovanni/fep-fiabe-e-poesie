fb53fff685758e2f3ee27fe1bf2ce5ac806fbdde
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-11-04 22:44:43 +0100

    changed backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  FEP_BACKEND: "https://fep-backend.herokuapp.com"
+  // FEP_BACKEND: "https://fep-backend.herokuapp.com",
+  FEP_BACKEND: 'https://fep-backend.vercel.app',
 };
