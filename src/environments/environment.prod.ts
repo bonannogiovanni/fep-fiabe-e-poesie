6e11c371446fd35fc088556dd36fd2291db03632
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-11-04 23:13:21 +0100

    new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // FEP_BACKEND: "https://fep-backend.herokuapp.com",
-  FEP_BACKEND: 'https://fep-backend.vercel.app',
+  FEP_BACKEND: 'https://fep-backend.onrender.com',
 };
